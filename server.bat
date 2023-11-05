54bf76718998eb7ad36ed105f70185ee67dcc46a
Nehir <105875351+catriverr@users.noreply.github.com>
2023-11-05 10:38:54 +0300

    Update server.bat

--- server.bat
+++ server.bat
@@ -1,1 +1,1 @@
-start cs2.exe -dedicated -insecure +game_type 2 +game_mode 0 +map de_dust2 +servercfgfile server.cfg
+start cs2.exe -dedicated -insecure +game_type 2 +game_mode 0 +map de_mirage +servercfgfile config.cfg
